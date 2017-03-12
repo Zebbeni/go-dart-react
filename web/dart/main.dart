330d3612f169071848e2c019070db6144384b2ff
Zebbeni <andrew.albers@gmail.com>
2017-03-12 11:32:30 -0600

    add component example from dart-react documentation

--- web/dart/main.dart
+++ web/dart/main.dart
@@ -1,10 +1,30 @@
 import 'dart:html';
-import 'package:react/react.dart' as react;
+import 'package:react/react.dart';
 import 'package:react/react_client.dart' as react_client;
 import 'package:react/react_dom.dart' as react_dom;
 
+typedef AppType({String headline, String text});
+
+var _app = registerComponent(() => new App());
+
+AppType app = ({headline, text}) =>
+    _app({'headline':headline, 'text':text});
+
+class App extends Component {
+  get headline => props['headline'];
+  get text => props['text'];
+  render() =>
+      div({},[
+        h1({}, headline),
+        span({}, text),
+      ]);
+}
+
 void main() {
   react_client.setClientConfiguration();
-  var component = react.h1({}, 'Updated from Dart and React');
-  react_dom.render(component, querySelector('#content'));
+  react_dom.render(
+      app(headline: "My custom headline",
+          text: "My custom text"),
+      querySelector('#app')
+  );
 }
