33f2900ef30f49a81a56d9f1935c5e6ee4faf0a9
Zebbeni <andrew.albers@gmail.com>
2017-03-12 16:10:16 -0600

    still messy but round trip working

--- app.go
+++ app.go
@@ -14,16 +14,16 @@ func init() {
 }
 
 type Response struct {
-    RandSmall int   `json:"randSmall"`
-    RandBig   int   `json:"randBig"`
+    RandLeft  int   `json:"randLeft"`
+    RandRight int   `json:"randRight"`
 }
 
 func funcHandler(w http.ResponseWriter, r *http.Request) {
-    randSmall := rand.Intn(10)
-    randBig := rand.Intn(10)
+    randLeft := rand.Intn(10)
+    randRight := rand.Intn(10) + 10
     response := &Response{
-        RandSmall: randSmall,
-        RandBig: randBig,
+        RandLeft:  randLeft,
+        RandRight: randRight,
     }
     responseJson, _ := json.Marshal(response)
     fmt.Fprintln(w, string(responseJson))
