a0629d2cdb31ce08ff1f6564963a4f8893b91780
Zebbeni <andrew.albers@gmail.com>
2017-03-12 09:36:03 -0600

    add react

--- web/dart/main.dart
+++ web/dart/main.dart
@@ -1,5 +1,10 @@
 import 'dart:html';
+import 'package:react/react.dart' as react;
+import 'package:react/react_client.dart' as react_client;
+import 'package:react/react_dom.dart' as react_dom;
 
 void main() {
-  querySelector('#header2').innerHtml = 'Updated from Dart';
+  react_client.setClientConfiguration();
+  var component = react.h1({}, 'Updated from Dart and React');
+  react_dom.render(component, querySelector('#content'));
 }
